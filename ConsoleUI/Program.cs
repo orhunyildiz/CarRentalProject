0e7469bd1d6aeb792e20f6cca54fb9f0fdba6369
Orhun YILDIZ <orhunyyildiz@gmail.com>
2021-05-23 11:02:11 +0300

    Project Updated

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,8 +9,29 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //CarTest();
             CarManager carManager = new CarManager(new EfCarDal());
-            foreach (var car in carManager.GetCarsByColorId(1))
+            foreach (var car in carManager.GetCarDetails())
+            {
+                Console.WriteLine("Araba: " + car.Description + " Marka: " + car.BrandName + " Renk: " + car.ColorName + " Günlük Ücret: " + car.DailyPrice);
+            }
+        }
+
+        private static void CarTest()
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                Console.WriteLine(car.Description);
+            }
+            Console.WriteLine("**********************");
+            Console.WriteLine(carManager.GetById(4).Description);
+            Console.WriteLine("**********************");
+            //carManager.Add(new Entities.Concrete.Car {DailyPrice = 5, ModelYear = 2015, Description = "deneme", BrandId = 3, ColorId = 2 });
+            Console.WriteLine("**********************");
+            //carManager.Delete(new Entities.Concrete.Car { Id = 1004});
+            carManager.Update(new Entities.Concrete.Car { Id = 1006, BrandId = 1, ColorId = 3, DailyPrice = 7, Description = "güncel", ModelYear = 145 });
+            foreach (var car in carManager.GetAll())
             {
                 Console.WriteLine(car.Description);
             }
